0dcfd5bca673859f19a9913f45417aac60c5d9f7
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-02-20 11:35:35 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,37 @@
+const os = process
+global.DeveloperMode = 'false' //true Or false
+global.owner = ['6282160268332', '6282128475388'] || os.env["6282160268332","6282128475388"]// Put your number here
+global.linkGC = ['http://bit.ly/Grupgabutbotv1']
+global.mods = ['0'] // Want some help?
+global.prems = ['6282160268332', '6282128475388'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  vhtear: 'https://vhtear.com',
+  zeks: 'https://api.zeks.xyz',
+  clph: 'https://recoders-area.caliph.repl.co'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '7a99fbd6b77336d5',
+  'https://recoders-area.caliph.repl.co': 'FreeApi',
+  'https://vhtear.com': 'AdiOfficial404',
+  'https://api.zeks.xyz': 'apivinz'
+}
+
+// Sticker WM
+global.packname = 'YNTKTS'
+global.author = ' '
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
